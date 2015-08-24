86eab9ef3ce2a2b7483a09d4a3d109c10707c7f2
dipe2003 <dipe.2003@hotmail.com>
2015-08-24 17:49:39 -0300

    Se agrego el ControladorEstudiante.

---
+++ ProyectoTISJ-ejb/src/java/Estudiante/ControladorEstudiante.java
@@ -0,0 +1,64 @@
+
+package Estudiante;
+
+import java.util.List;
+import javax.ejb.EJB;
+
+
+public class ControladorEstudiante {
+    @EJB
+    ManejadorEstudiante mEst;
+    
+    /**
+     * Crea un Estudiante y lo persiste.
+     * @param FormInscripcion 
+     * @param NombreUsuario
+     * @param ApellidoUsuario
+     * @param CorreoUsuario
+     * @param PasswordUsuario
+     * @param CedulaIdentidadUsuario
+     * @return Devulve un Admininstrador si fue creado, de lo contrario devuelve null.
+     */
+    public Estudiante CrearEstudiante(String FormInscripcion, String NombreUsuario, String ApellidoUsuario, String CorreoUsuario, String PasswordUsuario,  int CedulaIdentidadUsuario){
+        Estudiante est = new Estudiante(FormInscripcion, NombreUsuario, ApellidoUsuario, CorreoUsuario, PasswordUsuario,  CedulaIdentidadUsuario);
+        if (mEst.CrearEstudiante(est)!=-1) {
+            return est;
+        }
+        return null;
+    }
+    
+    /**
+     * Modifica los datos de un Estudiante en la base de datos.
+     * @param estudiante
+     * @return Devuelve -1 si no se pudo actualizar.
+     */
+    public int ModificarEstudiante(Estudiante estudiante){
+        return mEst.ModificarEstudiante(estudiante);
+    }
+    
+    /**
+     * Borra un Estudiante de la base de datos.
+     * @param estudiante
+     * @return Devuelve -1 si no se pudo borrar.
+     */
+    public int BorrarEstudiante(Estudiante estudiante){
+        return mEst.BorrarEstudiante(estudiante);
+    }
+    
+    /**
+     * Busca un Admininstrador en la base de datos.
+     * @param id
+     * @return Devuelve null si el Estudiante no se pudo encontrar.
+     */
+    public Estudiante BuscarEstudiante(int id){
+        return mEst.BuscarEstudiante(id);
+    }
+    
+    /**
+     * Devuelve una lista de Estudiantes desde la base de datos.
+     * @return 
+     */
+    public List<Estudiante> ListarEstudiantees(){
+        return mEst.ListarEstudiantes();
+    }
+}
--- ProyectoTISJ-ejb/src/java/Estudiante/Estudiante.java
+++ ProyectoTISJ-ejb/src/java/Estudiante/Estudiante.java
@@ -18,13 +18,24 @@ public class Estudiante extends Usuario {
     @OneToMany
     private List<Encuesta> EncuestasEstudiante;
 
-    public Estudiante(String FormInscripcion) {this.FormInscripcion = FormInscripcion;}
+    public Estudiante(String FormInscripcion, String NombreUsuario, String ApellidoUsuario, String CorreoUsuario, String PasswordUsuario, int CedulaUsuario) {
+        super(NombreUsuario, ApellidoUsuario, CorreoUsuario, PasswordUsuario, CedulaUsuario);
+        this.FormInscripcion = FormInscripcion;
+    }
 
     public Estudiante() {}
 
     public String getFormInscripcion() {return FormInscripcion;}
 
     public void setFormInscripcion(String FormInscripcion) {this.FormInscripcion = FormInscripcion;}
+
+    public List<Evaluacion> getEvaluacionesEstudiante() {return EvaluacionesEstudiante;}
+
+    public void setEvaluacionesEstudiante(List<Evaluacion> EvaluacionesEstudiante) {this.EvaluacionesEstudiante = EvaluacionesEstudiante;}
+
+    public List<Encuesta> getEncuestasEstudiante() {return EncuestasEstudiante;}
+
+    public void setEncuestasEstudiante(List<Encuesta> EncuestasEstudiante) {this.EncuestasEstudiante = EncuestasEstudiante;}    
     
     
 }
