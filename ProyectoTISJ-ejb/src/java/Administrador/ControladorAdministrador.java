56a392f040bad9c7e354bbd00876d97bdb4397ac
dipe2003 <dipe.2003@hotmail.com>
2015-08-24 17:27:20 -0300

    Se agrego el ControladorAdministrador.

---
+++ ProyectoTISJ-ejb/src/java/Administrador/ControladorAdministrador.java
@@ -0,0 +1,63 @@
+
+package Administrador;
+
+import java.util.List;
+import javax.ejb.EJB;
+
+
+public class ControladorAdministrador {
+    @EJB
+    ManejadorAdmininstrador mAdmin;
+    
+    /**
+     * Crea un Administrador y lo persiste.
+     * @param NombreUsuario
+     * @param ApellidoUsuario
+     * @param CorreoUsuario
+     * @param PasswordUsuario
+     * @param CedulaIdentidadUsuario
+     * @return Devulve un Admininstrador si fue creado, de lo contrario devuelve null.
+     */
+    public Administrador CrearAdministrador(String NombreUsuario, String ApellidoUsuario, String CorreoUsuario, String PasswordUsuario,  int CedulaIdentidadUsuario){
+        Administrador admin = new Administrador(NombreUsuario, ApellidoUsuario, CorreoUsuario, PasswordUsuario,  CedulaIdentidadUsuario);
+        if (mAdmin.CrearAdministrador(admin)!=-1) {
+            return admin;
+        }
+        return null;
+    }
+    
+    /**
+     * Modifica los datos de un Administrador en la base de datos.
+     * @param administrador
+     * @return Devuelve -1 si no se pudo actualizar.
+     */
+    public int ModificarAdministrador(Administrador administrador){
+        return mAdmin.ModificarAdministrador(administrador);
+    }
+    
+    /**
+     * Borra un Administrador de la base de datos.
+     * @param administrador
+     * @return Devuelve -1 si no se pudo borrar.
+     */
+    public int BorrarAdministrador(Administrador administrador){
+        return mAdmin.BorrarAdministrador(administrador);
+    }
+    
+    /**
+     * Busca un Admininstrador en la base de datos.
+     * @param id
+     * @return Devuelve null si el Administrador no se pudo encontrar.
+     */
+    public Administrador BuscarAdministrador(int id){
+        return mAdmin.BuscarAdministrador(id);
+    }
+    
+    /**
+     * Devuelve una lista de Administradores desde la base de datos.
+     * @return 
+     */
+    public List<Administrador> ListarAdministradores(){
+        return mAdmin.ListarAdministradores();
+    }
+}
