9ed505866e6350fc211d1c539e9b24ed49e5f904
dipe2003 <dipe.2003@hotmail.com>
2015-08-29 17:08:47 -0300

    Se agrego de nuevo Login.

---
+++ ProyectoTISJ-war/src/java/interfaz/Login.java
@@ -0,0 +1,75 @@
+package interfaz;
+
+import Usuario.FacadeUsuario;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.faces.bean.SessionScoped;
+import javax.inject.Named;
+
+@Named
+@SessionScoped
+@Stateless
+public class Login {
+   private String Usuario;
+   private String Password;
+   private List<String> Roles;
+   private String RolSeleccionado;
+   
+   @EJB
+   private FacadeUsuario fUsr;
+
+    public Login() {
+    }
+
+    public String getRolSeleccionado() {
+        return RolSeleccionado;
+    }
+
+    public void setRolSeleccionado(String RolSeleccionado) {
+        this.RolSeleccionado = RolSeleccionado;
+    }
+
+    public String getUsuario() {
+        return Usuario;
+    }
+
+    public void setUsuario(String Usuario) {
+        this.Usuario = Usuario;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+
+    public List<String> getRoles() {
+        return Roles;
+    }
+
+    public void setRoles(List<String> Roles) {
+        this.Roles = Roles;
+    }
+    
+    public String login(){
+        if (fUsr.ExisteUsuario(Usuario, Password, RolSeleccionado)!= -1) {
+            System.out.println("Existe!!!!!!!!!!");
+        }        
+        return "logueado";
+    }
+   
+    @PostConstruct
+    public void Init(){
+        this.Roles = new ArrayList<>();
+        Roles.add("Administrador");
+        Roles.add("Administrativo");
+        Roles.add("Docente");
+        Roles.add("Estudiante");
+    }
+   
+}
