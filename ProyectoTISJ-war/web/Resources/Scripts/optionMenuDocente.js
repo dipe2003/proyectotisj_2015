e093cbd75002017232b17d7f134ed63ebbaacaec
imsrnfo <ig_si@hotmail.com>
2015-09-30 20:23:56 -0300

    se agrego el efecto fade in de los campos

--- ProyectoTISJ-war/web/Resources/Scripts/optionMenuDocente.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $( ".imagenDocente" ).mouseenter(function() {
-        $('.optionContainer').stop().fadeIn(200);
-    }).mouseleave(function() {
-        $('.optionContainer').stop().fadeOut(200);
-    });
-});
