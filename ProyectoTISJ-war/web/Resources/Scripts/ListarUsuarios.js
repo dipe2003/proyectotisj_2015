7579ff62e771dd6bf94a10ef6067c69a7d68d70b
imsrnfo <ig_si@hotmail.com>
2015-09-30 17:18:30 -0300

    se agrego el efecto fade in de los campos

---
+++ ProyectoTISJ-war/web/Resources/Scripts/ListarUsuarios.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){    
+	$( ".ListItem" ).click(function() {
+		if ($(this).hasClass("ListItemClick")){
+			$(this).removeClass("ListItemClick");
+			$( this ).parent().animate({
+				height: 50
+			}, 200, function() {
+				//fin de la animacion
+			}); 
+		}else{
+			$( this ).addClass("ListItemClick");
+			$( this ).parent().animate({
+				height: 300
+			}, 200, function() {
+				//fin de la animacion
+			}); 
+		}
+	});
+	
+	var tiempo=0;
+	$('.ListItemContainer').hide().each(function() {
+		$(this).delay(tiempo).fadeIn('slow');
+		tiempo += 200;
+	});
+});   
