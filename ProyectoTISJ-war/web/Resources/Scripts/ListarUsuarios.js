f03f0cd30201ec4940159816a54a65faf4a76fad
dipe2003 <dipe.2003@hotmail.com>
2015-10-01 19:13:30 -0300

    Se corrigio error que no permitia registrar los estudios cursados de un estudiante. Se agrego loginRol para comenzar con caso de uso de login automatico para los usuarios con un unico rol. Se agregaron comentarios a UsuarioListdoBean y DatosUsuarioBean. Se quito el commandButton para registrar usuario quedando solo el commandLink.

--- ProyectoTISJ-war/web/Resources/Scripts/ListarUsuarios.js
+++ ProyectoTISJ-war/web/Resources/Scripts/ListarUsuarios.js
@@ -1,25 +1,26 @@
 $(document).ready(function(){    
-	$( ".ListItem" ).click(function() {
-		if ($(this).hasClass("ListItemClick")){
-			$(this).removeClass("ListItemClick");
-			$( this ).parent().animate({
-				height: 50
-			}, 200, function() {
-				//fin de la animacion
-			}); 
-		}else{
-			$( this ).addClass("ListItemClick");
-			$( this ).parent().animate({
-				height: 300
-			}, 200, function() {
-				//fin de la animacion
-			}); 
-		}
-	});
-	
-	var tiempo=0;
-	$('.ListItemContainer').hide().each(function() {
-		$(this).delay(tiempo).fadeIn('slow');
-		tiempo += 200;
-	});
-});   
+    $( ".ListItem" ).click(function() {
+        if ($(this).hasClass("ListItemClick")){
+            $(this).removeClass("ListItemClick");
+            $( this ).parent().animate({
+                height: 50
+            }, 200, function() {
+                //fin de la animacion
+            }); 
+        }else{
+            $( this ).addClass("ListItemClick");
+            $( this ).parent().animate({
+                height: 300
+            }, 200, function() {
+                //fin de la animacion
+            }); 
+        }
+    });
+    
+    var tiempo=0;
+    $('.ListItemContainer').hide().each(function() {
+        $(this).delay(tiempo).fadeIn('slow');
+        tiempo += 200;
+    });
+    
+});
--- ProyectoTISJ-war/web/Resources/Scripts/ScrollBar.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-    $('.asignaturaContainer').slimScroll({
-        height: '150px'
-    });
-});
